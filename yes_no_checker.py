e159d8892f13decb97a84b609e02e000e0d311c7
SefoV2231 <22321@fraser.school.nz>
2025-02-27 13:13:10 +1300

    Code ends when user inputs either Yes, y or no, n regardless of caps
    Update #4

--- yes_no_checker.py
+++ yes_no_checker.py
@@ -3,8 +3,7 @@ def yes_no(question): #defines the yes_no question
     
         response= input(question).lower() #converts the question to lowercase
         if response == 'yes' or response == 'y': #If the user returns either yes or letter y
-            return 'Yes'
-            
+            return 'Yes'   
         elif response == 'no' or response == 'n': #If the user returns either no or letter n
             return 'No'
             
@@ -13,6 +12,4 @@ def yes_no(question): #defines the yes_no question
 
 while True: #loops the question
     want_instructions = yes_no('Do you want to read the instructions? ')
-    print(f"You chose {want_instructions}\n") 
-    break
-    
+    print(f"You chose {want_instructions}") 
