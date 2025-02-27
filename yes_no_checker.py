ddc06b015b640756cba26d7ab98c98cd89de16f0
SefoV2231 <22321@fraser.school.nz>
2025-02-27 13:06:22 +1300

    Attempting to break code when user enters yes or no
    Updated #3

--- yes_no_checker.py
+++ yes_no_checker.py
@@ -4,8 +4,10 @@ def yes_no(question): #defines the yes_no question
         response= input(question).lower() #converts the question to lowercase
         if response == 'yes' or response == 'y': #If the user returns either yes or letter y
             return 'Yes'
+            break
         elif response == 'no' or response == 'n': #If the user returns either no or letter n
             return 'No'
+            break
         else:
             print('Please enter yes (y) or no (n).\n') #Goes back to the start if yes or no is not entered
 
